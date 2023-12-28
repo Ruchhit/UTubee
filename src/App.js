40be57648d4b599e490fbeab63ce73ff217c68ea
Ruchhit <ruchitgule2219@gmail.com>
2023-12-28 20:14:04 +0530

    installation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ import React from 'react'
+import Header from './components/Header'
+import Body from './components/Body'
+ 
+ const App = () => {
+   return (
+     <div className='bg-green-500'>
+     <Header/>
+     <Body/>
+     </div>
+   )
+ }
+ 
+ export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-red-700'>
+        <div>
+            Header
+        </div>
+    </div>
+  )
+}
+
+export default Header
