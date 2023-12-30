b0b11ab0586b2f546601fd0d83f2abd3bb9971e1
Ruchhit <ruchitgule2219@gmail.com>
2023-12-30 19:03:48 +0530

    update : video card routing

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_API_KEY } from '../utils/constants';
 import VideoCard from './VideoCard';
+import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
 
@@ -16,9 +17,11 @@ const VideoContainer = () => {
     },[])
   return (
     <div className='rounded-lg grid grid-cols-5 grid-flow-row gap-2 p-2 m-2'>
-        {/* <VideoCard info={videos[0]}/> */}
         { videos.map((video)=>(
-            <VideoCard key={video.id} info={video}/>
+            <Link key={video.id} to={"/watch?v="+video.id}>
+                <VideoCard info={video}/>
+            </Link>
+        
         ))}
     </div>
   )
